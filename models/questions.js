b2bd8d92d2061ec553c0c91b647258a784f58da8
jangollakeshav <113223848+jangollakeshav@users.noreply.github.com>
2023-01-09 09:10:19 +0530

    Update questions.js

--- models/questions.js
+++ models/questions.js
@@ -1,5 +1,5 @@
 "use strict";
-const { Model } = require("sequeliz");
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class questions extends Model {
     static countquestions(electionID) {
