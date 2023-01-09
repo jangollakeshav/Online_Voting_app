baf790541321de9b0a51db50654fba8b8e850108
jangollakeshav <113223848+jangollakeshav@users.noreply.github.com>
2023-01-09 08:57:23 +0530

    Update 20221213125041-create-questions.js

--- migrations/20221213125041-create-questions.js
+++ migrations/20221213125041-create-questions.js
@@ -27,6 +27,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("question");
+    await queryInterface.dropTable("questions");
   },
 };
