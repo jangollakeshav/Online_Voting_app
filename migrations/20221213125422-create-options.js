628c91012d83bab7feec792fb79faa4be8431082
jangollakeshav <113223848+jangollakeshav@users.noreply.github.com>
2023-01-09 08:58:36 +0530

    Update 20221213125422-create-options.js

--- migrations/20221213125422-create-options.js
+++ migrations/20221213125422-create-options.js
@@ -24,6 +24,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("options");
+    await queryInterface.dropTable("option");
   },
 };
