66e28f4dec55c04f321e356e5632dbfda49dd885
jangollakeshav <113223848+jangollakeshav@users.noreply.github.com>
2023-01-09 09:02:15 +0530

    Update 20221215114142-adding-electionIDto-voters.js

--- migrations/20221215114142-adding-electionIDto-voters.js
+++ migrations/20221215114142-adding-electionIDto-voters.js
@@ -1,6 +1,6 @@
 "use strict";
 
-/** @type {import('sequelize-cli').Migratio} */
+/** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
     /**
